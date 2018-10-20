1d4e9c7c61adf24e1cbbedef300a00693bfd6323
Brian Francis <13bfrancis@gmail.com>
2018-10-20 13:41:17 -0400

    named queries

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider, Query } from "react-apollo";
 import gql from "graphql-tag";
 
 const client = new ApolloClient({
   uri: "https://api-useast.graphcms.com/v1/cjnhocb7y1s5801ij2oocs0vv/master"
 });
 
-const testQuery = gql`
-  {
+const POSTS_QUERY = gql`
+  query allPosts {
     posts {
       id
       title
@@ -18,17 +18,23 @@ const testQuery = gql`
   }
 `;
 
-client
-  .query({
-    query: testQuery
-  })
-  .then(res => console.log(res));
+// client
+//   .query({
+//     query: testQuery
+//   })
+//   .then(res => console.log(res));
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <h1>hello world</h1>
+        <Query query={POSTS_QUERY}>
+          {({ loading, data }) => {
+            if (loading) return "Loading ...";
+            const { posts } = data;
+            return posts.map(posts => <h1 key={posts.id}>{posts.title}</h1>);
+          }}
+        </Query>
       </ApolloProvider>
     );
   }
